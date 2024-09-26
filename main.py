16be13e280fa7efc940534a2a1588ba9a2cd2a01
andersonfreitas-art <freitas0129@gmail.com>
2024-09-26 13:36:45 -0300

    Ajustando árvore de arquivos do projeto inicial e refazendo o arquivo requirements

--- main.py
+++ main.py
@@ -1,24 +1,63 @@
 from fasthtml.common import *
-from pages import home
 
 app, rt = fast_app(
-    debug=True,
+    debug=False,
     static_path="assets",
     pico=True,
-    live=True,
+    live=False,
 )
-
-home = home.page
 
 
 @rt("/")
 def get():
-    return home
-
-
-@rt("/{name}")
-def get(name: str):
-    return Titled(f"Seja bem vindo {name}", "É bom te ver aqui.")
+    page = (
+        Title("Agenda Fácil"),
+        Header(
+            Hgroup(
+                H1("Agenda Fácil 📅"),
+                Nav(
+                    Ul(Li(Strong("EEEP Sandra Carvalho Costa"))),
+                    Ul(Li(A("Conta", cls="contrast")), Li(A("Sair", cls="contrast"))),
+                ),
+                Hr(),
+            ),
+            cls="container",
+        ),
+        Main(
+            Section(
+                Group(
+                    Img(src="icon-projetor.png", width="50"),
+                    Img(src="icon-projetor.png", width="50"),
+                    Img(src="icon-projetor.png", width="50"),
+                    Img(src="icon-projetor.png", width="50"),
+                    Img(src="icon-projetor.png", width="50"),
+                    Img(src="icon-projetor.png", width="50"),
+                ),
+                Group(
+                    Img(src="icon-projetor.png", width="50"),
+                    Img(src="icon-tv.png", width="50"),
+                    Img(src="icon-tv.png", width="50"),
+                    Img(src="icon-linguas.png", width="50"),
+                    Img(src="icon-fisica.png", width="50"),
+                    Img(src="icon-matematica.png", width="50"),
+                ),
+                Group(
+                    Img(src="icon-biologia.png", width="50"),
+                    Img(src="icon-quimica.png", width="50"),
+                    Img(src="icon-auditorio.png", width="50"),
+                    Img(src="icon-biblioteca.png", width="50"),
+                    Img(src="icon-quadra.png", width="50"),
+                ),
+            ),
+            cls="container",
+        ),
+        Footer(
+            Hr(),
+            Small("© Agenda Fácil 2024 · Por Anderson Freitas"),
+            cls="container",
+        ),
+    )
+    return page
 
 
 serve()
